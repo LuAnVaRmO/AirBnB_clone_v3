d1febcc318a2d8937cf674d7f12e683a7ce2cfb2
AMIRANDA9112 <andreselfm@gmail.com>
2020-09-10 16:49:13 -0500

    get, put, delete post

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,8 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.state import State
+from models.city import City
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -15,10 +17,10 @@ def route():
 def stats():
     """ Retrieves the number of each objects by type """
     return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
+                    "cities": storage.count(City),
                     "places": storage.count("Place"),
                     "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
+                    "states": storage.count(State),
                     "users": storage.count("User")})
 
 
