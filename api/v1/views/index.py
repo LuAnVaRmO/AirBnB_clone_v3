324e09890a45a650f22422982dd94071c73262ae
Luis Angel Vargas Mosquera <luisangelvargas91@gmail.com>
2020-09-10 09:44:50 -0500

    Task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,5 +11,16 @@ def route():
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Retrieves the number of each objects by type """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("City"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
+
+
 if __name__ == "__main__":
     pass
