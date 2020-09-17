15f87a22cd349d1279150b7910efacf5c4d3b667
AMIRANDA9112 <andreselfm@gmail.com>
2020-09-16 23:19:25 -0500

    states resolve

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,86 +1,71 @@
 #!/usr/bin/python3
-""" States API module """
-from . import app_views
-from flask import jsonify, request, abort
+""" place reviews API module """
+from models.state import State
+from api.v1.views import app_views
+from flask import jsonify, abort, request
 from models import storage
-from models.place import Place
-from models.review import Review
-from models.user import User
 
 
-@app_views.route('/places/<place_id>/reviews', methods=['GET'],
-                 strict_slashes=False)
-def get_review_by_place(place_id=None):
-    """ get all reviews by place request """
-    list_rev = []
-    all_places = storage.get(Place, place_id)
-    if all_places:
-        rev = place.reviews
-        for value in rev:
-            list_.append(value.to_dict())
-        return jsonify(list_rev), 200
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_states(state_id=None):
+    """Retrieves the list of all States
+    """
+    list_ = []
+    all_states = storage.all(State)
+    if state_id:
+        state = all_states.get("State." + state_id, None)
+        return state.to_dict() if state else abort(404)
     else:
-        abort(404)
+        for value in all_states.values():
+            list_.append(value.to_dict())
+        return jsonify(list_), 200
 
 
-@app_views.route('/reviews/<review_id>', methods=['GET'],
+@app_views.route('/states/<state_id>', methods=['DELETE'],
                  strict_slashes=False)
-def get_review_by_id(review_id=None):
-    """ get review by ID """
-    rev = storage.get(Review, review_id)
-    return rev.to_dict() if rev else abort(404)
-
-
-@app_views.route('/places/<place_id>/reviews', methods=['POST'],
-                 strict_slashes=False)
-def post_review(place_id=None):
-    """ send review """
-    body = request.get_json()
-
-    place = storage.get(Place, place_id)
-    if not place:
-        abort(404)
-    if not request.is_json:
-        return jsonify(error='Not a JSON'), 400
-    if "user_id" not in body.keys():
-        return jsonify(error="Missing user_id"), 400
-
-    user = storage.get(User, body["user_id"])
-    if not user:
-        abort(404)
-    if "text" not in body.keys():
-        return jsonify(error="Missing text"), 400
-    rev = Review(place_id=place_id, **body)
-    rev.save()
-    return jsonify(rev.to_dict()), 201
-
-
-@app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
-def put_review(review_id=None):
-    """ Update review """
-    review = storage.get(Review, review_id)
-    body_dic = request.get_json()
-    if not review:
-        abort(404)
-    elif not request.is_json:
-        return jsonify({'error': 'Not a JSON'}), 400
-    for key, value in body_dic.items():
-        if key in ['id', 'user_id', 'place_id', 'updated_at', 'updated_at',
-                   'state_id']:
-            continue
-        setattr(review, key, value)
-    review.save()
-    return jsonify(review.to_dict()), 200
-
-
-@app_views.route('/reviews/<review_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def delete_review(review_id=None):
-    """ Delete review by id """
-    rev = storage.get(Review, review_id)
-    if rev:
-        storage.delete(rev)
+def delete_state(state_id=None):
+    """Deletes a State
+    """
+    state = storage.get(State, state_id)
+    if state:
+        storage.delete(state)
         storage.save()
         return jsonify({}), 200
     else:
         abort(404)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def save_state():
+    """Creates a State
+    """
+    body = request.get_json()
+    if request.is_json:
+        if "name" in body.keys():
+            state = State(**body)
+            state.save()
+            return jsonify(state.to_dict()), 201
+        else:
+            return jsonify(error="Missing name"), 400
+    return jsonify(error='Not a JSON'), 400
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id=None):
+    """Updates a Amenity object
+    """
+    body = request.get_json()
+    all_states = storage.all(State)
+    state = all_states.get("State." + state_id, None)
+    if state is None:
+        abort(404)
+    elif not request.is_json:
+        return jsonify(error='Not a JSON'), 400
+
+    for m, n in body.items():
+        if m in ['id', 'updated_at', 'updated_at']:
+            continue
+        setattr(state, m, n)
+    state.save()
+    return jsonify(state.to_dict()), 200
