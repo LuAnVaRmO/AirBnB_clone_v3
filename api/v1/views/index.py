a4caca008f59f760587046bf261f82bad45d44cc
AMIRANDA9112 <andreselfm@gmail.com>
2020-09-16 23:04:41 -0500

    states resolve

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -22,10 +22,6 @@ def stats():
     return jsonify({"amenities": storage.count(Amenity),
                     "cities": storage.count(City),
                     "places": storage.count(Place),
-                    "reviews": storage.count("Review"),
+                    "reviews": storage.count(Review),
                     "states": storage.count(State),
                     "users": storage.count(User)})
-
-
-if __name__ == "__main__":
-    pass
