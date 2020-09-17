f9bde92c0a2f07b86b29843e609c8cc64fe1d227
AMIRANDA9112 <andreselfm@gmail.com>
2020-09-16 23:56:01 -0500

    place reviews resolve

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
+from models.review import Review
 from models.user import User
 from models.place import Place
 
